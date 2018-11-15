b05403f6c846d502a5bb820a5af51d541c3dba98
Kenneth Hautea <32695245+KennethHautea@users.noreply.github.com>
2018-11-15 14:05:54 +0800

    initial commit
    navbar

---
+++ Davao-Durian-web/public/js/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBsYZUlt-gwhM-vujQ0l7HWFiRC_5M9xko",
+    authDomain: "u-hackademia-davao-durian.firebaseapp.com",
+    databaseURL: "https://u-hackademia-davao-durian.firebaseio.com",
+    projectId: "u-hackademia-davao-durian",
+    storageBucket: "u-hackademia-davao-durian.appspot.com",
+    messagingSenderId: "695278695243"
+  };
+  firebase.initializeApp(config);
